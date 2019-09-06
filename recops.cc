5df3293f097b38da83f5057421815059128c9345
Daniel Phillips <daniel@tux3.org>
2019-09-05 20:21:49 -0700

    Derive struct vh (variable size records from struct bh (fixed size records)
    In theory, we should now be able to construct a table using either bh, or
    vh, and the virtual functions should be different. What we want. Not sure
    this actually works yet.

--- recops.cc
+++ recops.cc
@@ -14,60 +14,16 @@ using namespace recops;
 
 struct bh : recinfo
 {
-	bh (void *data, unsigned size, unsigned reclen) : recinfo{(u8 *)data, size, reclen} {}
+	bh(void *data, unsigned size, unsigned reclen) : recinfo{(u8 *)data, size, reclen} {}
 
-	virtual int walk(rb_walk_fn fn, void *context)
-	{
-		return rb_walk(*this, fn, context);
-	}
+	#include "recops.inc"
+};
 
-	virtual int big()
-	{
-		return rb_big(*this);
-	}
+using namespace varops;
 
-	virtual int more()
-	{
-		return rb_more(*this);
-	}
+struct vh : bh
+{
+	vh(void *data, unsigned size, unsigned reclen) : bh(data, size, reclen) {}
 
-	virtual void dump()
-	{
-		rb_dump(*this);
-	}
-
-	virtual bool check()
-	{
-		return rb_check(*this);
-	}
-
-	virtual void *key(unsigned which, unsigned *len)
-	{
-		return rb_key(*this, which, len);
-	}
-
-	virtual rec_t *create(const void *key, u8 len, u16 lowhash, const void *data)
-	{
-		return rb_create(*this, key, len, lowhash, data, 0);
-	}
-
-	virtual rec_t *lookup(const void *key, u8 len, u16 lowhash)
-	{
-		return rb_lookup(*this, key, len, lowhash);
-	}
-
-	virtual int remove(const void *key, u8 len, u16 lowhash)
-	{
-		return rb_delete(*this, key, len, lowhash);
-	}
-
-	virtual void init()
-	{
-		rb_init(*this);
-	}
-
-//	virtual bool is_rb()
-//	{
-//		return ::is_rb(*this);
-//	}
+	#include "recops.inc"
 };
