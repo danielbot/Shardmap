95b8d84242485dec020122f4212f17ed59e691aa
Daniel Phillips <daniel@tux3.org>
2019-09-12 10:18:22 -0700

    Recops.cc cleanups

--- recops.cc
+++ recops.cc
@@ -1,9 +1,7 @@
-enum {maxname = 255};
-enum {cleanup = 0, cleaned = 0xee, deleted = 0xdd, holecode = 0xff};
-
+struct recinfo { const unsigned blocksize, reclen; u8 *data; loc_t loc; };
 struct tabent { u8 hash; u8 len; };
 
-struct rb
+struct rb // record block format
 {
 	u16 size; /* blocksize */
 	u16 used; /* entry headers and key text stored compactly at top of block */
@@ -15,26 +13,23 @@ struct rb
 };
 
 enum {tabent_size = sizeof(struct tabent), header_size = sizeof(struct rb)};
-
-struct recinfo { const unsigned blocksize, reclen; u8 *data; loc_t loc; };
+enum {cleanup = 0, cleaned = 0xee, deleted = 0xdd, holecode = 0xff, maxname = 255};
 
 typedef u8 rec_t;
-
 typedef void (rb_walk_fn)(void *context, u8 *key, unsigned keylen, u8 *data, unsigned reclen);
 
 static u8 rb_hash(u16 ihash) { return ihash % 255; }
 
-enum {taglen = 0}; // optional one byte variable data length borrowed from key
-
 struct ri : recinfo
 {
+	enum {taglen = 0}; // optional one byte variable data borrowed from key
 	ri(void *data, unsigned size, unsigned reclen) : recinfo{size, reclen, (u8 *)data} {}
 	#include "recops.inc"
 };
 
 struct vri : ri
 {
-	enum {taglen = 1};
+	enum {taglen = 1}; // record data can vary from reclen to reclen + 254
 	vri(void *data, unsigned size, unsigned reclen) : ::ri{data, size, reclen} {}
 	#include "recops.inc"
 };
